ee02c3f26b906b5711c7b0c5f0e1d1419cad5f92
Chris <cp1974@gmail.com>
2021-08-02 06:38:37 -0500

    proj checkin
    using wk 2 as a backup for main

--- groceries/src/app/tab1/tab1.page.ts
+++ groceries/src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { GroceriesServiceService } from '../groceries-service.service';
+import { InputDialogService } from '../input-dialog.service';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +12,57 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title = "Grocery";
+
+  constructor(public alertController: AlertController, 
+    public toastController: ToastController, 
+    public dataService: GroceriesServiceService, 
+    public inputDialogService: InputDialogService,
+    public socialSharing: SocialSharing) {}
+
+  loadItems() {
+    return this.dataService.getItems();
+  }
+
+  async removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = await this.toastController.create({
+      message: 'Removing Item - ' + item.name,
+      duration: 2000
+    });
+    toast.present();
+    this.dataService.removeItem(index);
+  }
+
+  async shareItem(item, index) {
+    console.log("Sharing Item - ", item, index);
+    const toast = await this.toastController.create({
+      message: 'Sharing Item - ' + item.name,
+      duration: 2000
+    });
+    toast.present();
+
+    let message = "Grocery Item - Name: " + item.nme + " Quantity: " + item.quantity;
+    let subject = "Sharfed via Groceries app";
+     // Check if sharing via email is supported
+    this.socialSharing.share(message, subject).then(() => {
+      // Sharing via email is possible
+      console.log("Share Success!");
+    }).catch(() => {
+      // Sharing via email is not possible
+      console.error("Share failed!");
+    });
+    //this.dataService.removeItem(index);
+  }
+
+  editItem(item, index) {
+    console.log("Editing Item - ", item, index);
+    this.inputDialogService.itemPrompt(item, index);
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.inputDialogService.itemPrompt();
+  }
 
 }
